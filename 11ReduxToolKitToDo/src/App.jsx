f256cb4b766868a2673d1e26df83d94702bca5bd
Rupjyoti Barman <rupjyoti.barman@hp.com>
2025-12-14 20:51:25 +0530

    created add and remove features using redux tool kit

--- 11ReduxToolKitToDo/src/App.jsx
+++ 11ReduxToolKitToDo/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
-
+import Todo from './components/Todo.jsx'
+import AddTodo from './components/AddTodo.jsx'
 function App() {
 
   return (
-    <>
-      <h1> Hellooo Haow Ki ha ?</h1>
-    </>
+    <div className="min-h-screen bg-gray-800 py-8">
+      <div className="container mx-auto px-4">
+        <div className="max-w-4xl mx-auto">
+          <h1 className="text-3xl font-bold text-center mb-8 text-white">ToDo Items</h1>
+          <AddTodo/>
+          <Todo/>
+        </div>
+      </div>
+    </div>
   )
 }
 
